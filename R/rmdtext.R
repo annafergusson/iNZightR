7dbcd2b7318ad7bd9a9a69d732869aad38b6a720
Anna Fergusson <annafergussonnz@gmail.com>
2018-01-13 18:09:35 +1300

    Update rmdtext.R

--- R/rmdtext.R
+++ R/rmdtext.R
@@ -3,12 +3,7 @@
 
 #' @export
 rmdtext <- function(text){
-  #writeLines(text, con="input.Rmd");
-  #knit2html("input.Rmd", output="output.html");
-  writeLines(text, con="input.Rmd")
-  t <- knit2html("input.Rmd", output="output.html")
-  invisible()
-  res <- readLines(t)
-  unlink(sub('.html$', '*', t))
-  paste(res, collapse = '\n')
+  writeLines(text, con="input.Rmd");
+  knit2html("input.Rmd", output="output.html");
+  invisible();
 }
