7f943da314b417a45c13b4cc6cc724b89067f9d0
Anna Fergusson <annafergussonnz@gmail.com>
2018-01-16 22:46:00 +1300

    Create autoplot.R

---
+++ R/autoplot.R
@@ -0,0 +1,20 @@
+#' @export
+autoplot <- function(x, y = NULL, g1 = NULL, g1.level = NULL, g2 = NULL,
+  g2.level = NULL, varnames = list(), colby = NULL, sizeby = NULL,
+  symbolby = NULL, extra.vars, locate = NULL, locate.id = NULL,
+  locate.col = NULL, locate.extreme = NULL, highlight = NULL,
+  data = NULL, design = NULL, freq = NULL, missing.info = TRUE,
+  xlab = varnames$x, ylab = varnames$y, new = TRUE, inzpars = inzpar(),
+  layout.only = FALSE, plot = TRUE, xaxis = TRUE, yaxis = TRUE,
+  xlim = NULL, ylim = NULL, zoombars = NULL, hide.legend = FALSE, df,
+  env = parent.frame()){
+  iNZightPlot(x, y = NULL, g1 = NULL, g1.level = NULL, g2 = NULL,
+  g2.level = NULL, varnames = list(), colby = NULL, sizeby = NULL,
+  symbolby = NULL, extra.vars, locate = NULL, locate.id = NULL,
+  locate.col = NULL, locate.extreme = NULL, highlight = NULL,
+  data = NULL, design = NULL, freq = NULL, missing.info = TRUE,
+  xlab = varnames$x, ylab = varnames$y, new = TRUE, inzpars = inzpar(),
+  layout.only = FALSE, plot = TRUE, xaxis = TRUE, yaxis = TRUE,
+  xlim = NULL, ylim = NULL, zoombars = NULL, hide.legend = FALSE, df,
+  env = parent.frame())
+  }
