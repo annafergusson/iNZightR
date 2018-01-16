b3e1c7eff6620211f5b72a8accd105cbf9c98518
Anna Fergusson <annafergussonnz@gmail.com>
2018-01-16 22:34:49 +1300

    Update rmdtext.R

--- R/rmdtext.R
+++ R/rmdtext.R
@@ -7,3 +7,24 @@ rmdtext <- function(text){
   knit2html("input.Rmd", output="output.html");
   invisible();
 }
+
+#' @export
+autoplot <- function(x, y = NULL, g1 = NULL, g1.level = NULL, g2 = NULL,
+  g2.level = NULL, varnames = list(), colby = NULL, sizeby = NULL,
+  symbolby = NULL, extra.vars, locate = NULL, locate.id = NULL,
+  locate.col = NULL, locate.extreme = NULL, highlight = NULL,
+  data = NULL, design = NULL, freq = NULL, missing.info = TRUE,
+  xlab = varnames$x, ylab = varnames$y, new = TRUE, inzpars = inzpar(),
+  layout.only = FALSE, plot = TRUE, xaxis = TRUE, yaxis = TRUE,
+  xlim = NULL, ylim = NULL, zoombars = NULL, hide.legend = FALSE, df,
+  env = parent.frame()){
+  iNZightPlot(x, y = NULL, g1 = NULL, g1.level = NULL, g2 = NULL,
+  g2.level = NULL, varnames = list(), colby = NULL, sizeby = NULL,
+  symbolby = NULL, extra.vars, locate = NULL, locate.id = NULL,
+  locate.col = NULL, locate.extreme = NULL, highlight = NULL,
+  data = NULL, design = NULL, freq = NULL, missing.info = TRUE,
+  xlab = varnames$x, ylab = varnames$y, new = TRUE, inzpars = inzpar(),
+  layout.only = FALSE, plot = TRUE, xaxis = TRUE, yaxis = TRUE,
+  xlim = NULL, ylim = NULL, zoombars = NULL, hide.legend = FALSE, df,
+  env = parent.frame())
+  }
